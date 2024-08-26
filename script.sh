d5c8693eeaa61c0fe36ef5ac2ec366a7b38712c3
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-08-26 10:01:09 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -23,6 +23,7 @@ echo "============="
 # Use AOSP clang for no errors thanks trijal kido.
 rm -r prebuilts/clang/host/linux-x86
 git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 prebuilts/clang/host/linux-x86
+echo "===== fix  AOSP clang for no errors ====="
 
 # Git cherry-pick
 cd vendor/lineage
@@ -31,6 +32,10 @@ git fetch los
 git cherry-pick 198966577ace63573e5be49e03a2e59e32997054
 cd ../..
 echo "===== Cherry-picking Success ====="
+
+# Private keys
+git clone https://github.com/Gtajisam/vendor_lineage-priv_keys.git vendor/lineage-priv/keys
+echo "===== cp clone done ====="
 
 # Export
 export BUILD_USERNAME=FARHAN_UN
