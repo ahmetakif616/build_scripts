6370ee7e2d0e65a0e7ebe30265ecb805b31bf075
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-08-30 22:00:43 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 rm -rf .repo/local_manifests/
 
 # repo init rom
-repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b QPR3 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 -u https://github.com/ProjectSakura/android.git -b 14 --git-lfs
 echo "=================="
 echo "Repo init success"
 echo "=================="
@@ -19,16 +19,8 @@ echo "============="
 echo "Sync success"
 echo "============="
 
-# Git cherry-pick
-cd vendor/lineage
-git remote add los https://github.com/LineageOS/android_vendor_lineage
-git fetch los
-git cherry-pick 198966577ace63573e5be49e03a2e59e32997054
-cd ../..
-echo "===== Cherry-picking Success ====="
-
 # Export
-export BUILD_USERNAME=FARHAN_UN
+export BUILD_USERNAME=FARHAN
 export BUILD_HOSTNAME=crave
 echo "======= Export Done ======"
 
@@ -37,6 +29,6 @@ echo "======= Export Done ======"
 echo "====== Envsetup Done ======="
 
 # Lunch
-lunch infinity_Mi439_4_19-userdebug || lunch infinity_Mi439_4_19-ap1a-userdebug 
+lunch lineage_Mi439_4_19-ap2a-userdebug  
 make installclean
 mka bacon
