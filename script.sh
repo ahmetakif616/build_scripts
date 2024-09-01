35dae8ce4022258179ca90303df7169c003dee20
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-09-02 00:09:14 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -9,7 +9,7 @@ echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/Gtajisan/local_manifests -b Los-21 .repo/local_manifests
+git clone https://github.com/Gtajisan/local_manifests -b Infinity_jp .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -19,18 +19,6 @@ echo "============================"
 echo "============="
 echo "Sync success"
 echo "============="
-
-# Git cherry-pick
-cd vendor/lineage
-git remote add los https://github.com/LineageOS/android_vendor_lineage
-git fetch los
-git cherry-pick 198966577ace63573e5be49e03a2e59e32997054
-cd ../..
-echo "===== Cherry-picking Success ====="
-
-# Private keys
-git clone https://github.com/Gtajisan/vendor_lineage-priv_keys.git vendor/lineage-priv/keys
-echo "===== cp clone done ====="
 
 # remove face unlock 
 rm -rf packages/apps/FaceUnlock
@@ -46,7 +34,7 @@ echo "====== Envsetup Done ======="
 
 # Lunch
 . build/envsetup.sh
-lunch infinity_Mi439-ap2a-userdebug || lunch infinity_Mi439-ap1a-userdebug 
+lunch infinity_Mi439-ap2a-userdebug
 
 # make install
 make installclean
