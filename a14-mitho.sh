6c8c256f2bc39346a23c2d2d31f0c2adbee418e4
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-09-02 16:08:06 +0600

    Update a14-mitho.sh

--- a14-mitho.sh
+++ a14-mitho.sh
@@ -1,12 +1,43 @@
-rm -rf .repo/local_manifests/
-#repo init rom
-repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
-#Local manifests
-git clone https://github.com/PhantomEnigma/local_manifests_clo -b udc-2 .repo/local_manifests
+#!/bin/bash
 
-#build
+rm -rf .repo/local_manifests/
+
+# repo init rom
+repo init --depth=1 -u https://github.com/AfterlifeOS/android_manifest.git -b 14 --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Local manifests
+git clone https://github.com/Gtajisan/local_manifests_clo -b Afterlife .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# build
 /opt/crave/resync.sh
+echo "============="
+echo "Sync success"
+echo "============="
+
+#  remove error 
+rm -rf external/chromium-webview  
+echo "======= remove Done ======"
+
+#  fix error 
+rm -rf packages/apps/ParanoidSense
+echo "=======  Done ======"
+
+# Export
+export BUILD_USERNAME=FARHAN•XD
+export BUILD_HOSTNAME=crave
+echo "======= Export Done ======"
+
+# Set up build environment
+source build/envsetup.sh
+echo "====== Envsetup Done ======="
+
+# lunch 
 . build/envsetup.sh
-riseup mi439 userdebug 
-make installclean
-rise b
+goafterlife Mi439_4_19
+goafterlife Mi439_4_19 userdebug
