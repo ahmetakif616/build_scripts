a3d82c4a5e203ef49d1a93fb63c2cbd16f811794
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-09-02 07:14:04 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -20,6 +20,14 @@ echo "============="
 echo "Sync success"
 echo "============="
 
+# Git cherry-pick
+cd vendor/infinity
+git remote add los https://github.com/LineageOS/android_vendor_lineage
+git fetch los
+git cherry-pick 198966577ace63573e5be49e03a2e59e32997054
+cd ../..
+echo "===== Cherry-picking Success ====="
+
 # remove face unlock 
 rm -rf packages/apps/FaceUnlock
 
